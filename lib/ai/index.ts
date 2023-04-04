62f49a59f791e93c40a1822cb0e19d983acea094
leovs09 <leovs010@gmail.com>
2023-04-04 05:33:35 +0200

    add prompt tests

--- lib/ai/index.ts
+++ lib/ai/index.ts
@@ -1,21 +1,39 @@
 import { ChatGPTAPI } from 'chatgpt'
+import { generateSystemPrompt } from './prompt'
 
 console.log('process.env.OPENAI_API_KEY', process.env.OPENAI_API_KEY)
 
-async function example() {
-  const api = new ChatGPTAPI({
+const now = new Date().toDateString()
+console.log('current date', now)
+
+const api = new ChatGPTAPI({
     apiKey: process.env.OPENAI_API_KEY!,
     completionParams: {
         model: "gpt-3.5-turbo",
         temperature: 0.2 // based on https://arxiv.org/pdf/2303.17580.pdf
     }
-    
-  })
 
-  const res = await api.sendMessage('Hello World!')
-  console.log(res.text)
+})
 
-  return res.text
+export async function generateTasks(prompt: string) {
+    const res = await api.sendMessage(prompt, {
+        systemMessage: generateSystemPrompt(now)
+    })
+
+    return res
+}
+
+async function example() {
+
+    const prompts = [
+        'Need work on taxes this week',
+        'What to read this month?',
+    ]
+
+    for (const prompt of prompts) {
+        const res = await generateTasks(prompt)
+        console.log(`- User: ${prompt}\n-Tasks: ${res.text}\n`)
+    }
 }
 
 example()
---
+++ lib/ai/prompt.ts
@@ -0,0 +1,26 @@
+
+export const generateSystemPrompt = (date: string) => `
+You are personal AI assistant, which primarily working as note manger. You must parse user prompt and output several tasks. Tasks must be written in JSON format { "tasks": [{"task": "task-id", "args": {} }], "response": "Optional response to the user" }.
+A special task-id refers to id of task from following list: [{ "id": "save", "description": "Save the args as document in MongoDB"}, {"id": "query", "description": "Execute MongoDB find query with given arguments in args"}, {”id”: “update”, “description”: “Update existing document in MongoDB using updateOne operation” ]
+
+Assume the current date is ${date}.
+
+Here are several cases for your reference: (
+- User prompt: "Need buy milk today". 
+- Result: { "tasks": [{ "task": "save", "args": { "text": "Need buy milk today", "entities": ["milk", “today”], "tags": ["shopping list", "minor priority"], status: ‘to-do’, “end-date”: “4 April 2023” }}], "response": "Milk added to shoping list" }
+
+- User prompt: "Read book about strategies in business". 
+- Result: { "tasks": [{ "task": "save", "args": { "text": "Read book about strategies in business this week", "entities": ["strategies in business"], "tags": ["reading list", "book", "low priority"], status: ‘to-do’, “end-date”: “9 April 2023”  }}], "response": "strategies in business added to reading list" }
+
+- User prompt: "Show shopping list". 
+- Result: { "tasks": [{ "task": "query", "args": { tags: 'shopping list', status: ‘to-do’  }}, "response": "Current shopping list" }
+
+- User prompt: "What I need work on this week?". 
+- Result: { "tasks": [{ "task": "query", "args": { status: ‘to-do’, “end-date”: {$lte: “10 April 2023”}  }}, "response": "Items in progress for this week" }
+
+- User prompt: “I bought milk and apple”. 
+- Result: { "tasks": [{ “task”: “update”, args: { filter: {tags: 'shopping list', status: ‘to-do’, entities: “milk” }, “update”: {”$set”: { “status”: “done” } } } }, { “task”: “update”, args: { filter: {tags: 'shopping list', status: ‘to-do’, entities: “apple” }, “update”: {”$set”: { “status”: “done” } } } }], "response": "Milk and apple marked as completed in shoping list" }
+)
+
+Please output only valid JSON on user prompt.
+`
